30e4ed1fe3796f9add160730f3decd7d95292cac
minstrelmage <tunkleo@umich.edu>
2022-01-20 12:02:53 -0500

    Implement check_end

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ const int O_VAL = 13;
 class TicTacToe {
     private:
         int boardValues[3][3] = {};
+        int valueSums[3][3] = {}; // row 0 rows; row 1 cols; row 2 diags and total; used for check_end
+        int valueCounts[3][3] = {}; // similar to valueSums
         int nextPlayer = X_VAL; // switch between 11 for X and 13 for O
     
     public:
@@ -44,12 +46,64 @@ void TicTacToe::next_move(int location){
 
     // since location passed the above checks, edit the board
     boardValues[row][col] = (location * nextPlayer);
+
+    // add information to valueSums and checkCounts for check_end to use
+    valueSums[0][row] += (location * nextPlayer); // update row info
+    valueCounts[0][row] += 1;
+    valueSums[1][col] += (location * nextPlayer); // update col info
+    valueCounts[1][col] += 1;
+    if (row == col) { // update forward diagonal info
+        valueSums[2][0] += (location * nextPlayer);
+        valueCounts[2][0] += 1;
+    }
+    if ((row + col) == 2) { // update backward diagonal info
+        valueSums[2][1] += (location * nextPlayer);
+        valueCounts[2][1] += 1;
+    }
+    valueSums[2][2] += (location * nextPlayer); // unused, but had one extra spot in the array lol
+    valueCounts[2][2] += 1;
+    
     nextPlayer = (nextPlayer == X_VAL) ? O_VAL : X_VAL; // toggle nextPlayer
     return;
 }
 
+int check_divisibility(int toCheck) {
+    if ((toCheck % X_VAL) == 0) {
+        return X_VAL;
+    } else if ((toCheck % O_VAL) == 0) {
+        return O_VAL;
+    } else {
+        return 0;
+    }
+}
+
 bool TicTacToe::check_end() {
-    return true; // TODO
+    for (int i = 0; i < 8; i++) {
+        int row = i / 3;
+        int col = i % 3;
+        if (valueCounts[row][col] == 3) {
+            int toCheck = valueSums[row][col];
+            if (check_divisibility(toCheck) == 0) {
+                continue;
+            } else if (check_divisibility(toCheck) == X_VAL) { // the row, col, or diag sum is divisible by X_VALUE
+                std::cout << "Congratulations Player X!" << std::endl;
+                return true;
+            } else if (check_divisibility(toCheck) == O_VAL) { // the row, col, or diag sum is divisible by O_VALUE
+                std::cout << "Congratulations Player O!" << std::endl;
+                return true;
+            } else { // hopefully won't happen
+                std::cout << "Something is screwy..." << std::endl;
+                return true;
+            }
+        }
+    }
+
+    if (valueCounts[2][2] == 9) { // if all 9 squares are occupied
+        std::cout << "It's a draw -_-" << std::endl;
+        return true;
+    }
+
+    return false; // continue game
 }
 
 // if a number is divisible by X_VAL or O_VAL, print X or O respectively
