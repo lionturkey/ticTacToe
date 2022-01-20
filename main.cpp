37adbfd860fe177b46817de29e525c72d45aba6c
minstrelmage <tunkleo@umich.edu>
2022-01-20 10:45:38 -0500

    Outlined TicTacToe class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+class TicTacToe {
+    private:
+        int boardValues[3][3] = {};
+        int nextTurn = 11; // switch between 11 for X and 13 for O
+    
+    public:
+        TicTacToe();
+        int getPlayer(); // show who gets the next turn
+        void next_move(int location); // update the board after input
+        int check_win(); // check if game is over
+        friend std::ostream & operator << (std::ostream &out, const TicTacToe &T); // for easy board printing
+}
+
+
 int main(){
     auto result = (10 <=> 20) > 0;
     std::cout << result << std::endl;
